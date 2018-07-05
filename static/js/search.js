37c5c511a10b5f8ab967cc01d8fff575fe58231b
rohitmusti <rohit.musti.rm@gmail.com>
2018-07-05 10:39:09 -0400

    made the linking beter

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,4 @@
-var documents = data;
 var cleanedResults = "<div class='results'>";
-
 var RESULTS = document.getElementById("results-wrapper");
 
 
@@ -8,7 +6,7 @@ var idx = lunr(function () {
   this.ref('href')
   this.field('content')
 
-  documents.forEach(function (doc) {
+  data.forEach(function (doc) {
     this.add(doc)
   }, this)
 })
@@ -17,8 +15,9 @@ var initialResults = idx.search('agile');
 
 for (var i = 0; i < initialResults.length; i++) {
 	console.log(initialResults[i].ref);
-	cleanedResults += "<p> https://rht-labs.github.io/practice-library/practices/" +  initialResults[i].ref + "</p>";
+	cleanedResults += "<a href='https://rht-labs.github.io/practice-library/practices/" +  initialResults[i].ref + "'>" + initialResults[i].ref + "</a><br />";
 }
+
 cleanedResults += "</div>";
 
 
