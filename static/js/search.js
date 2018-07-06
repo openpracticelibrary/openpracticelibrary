6b5ccf4dfa67a893080f013f8aaceb9bdd4f74a7
rohitmusti <rohit.musti.rm@gmail.com>
2018-07-06 14:01:59 -0400

    cleaned up the JS file

--- static/js/search.js
+++ static/js/search.js
@@ -21,7 +21,6 @@ var initialResults = idx.search(searchTerm);
 for (var i = 0; i < initialResults.length; i++) {
 	var res = initialResults[i].ref;
 	res = res.replace(/-/g, " ").replace(/([^a-z]|^)([a-z])(?=[a-z]{2})/g, function(_, g1, g2) { return g1 + g2.toUpperCase();});
-	console.log(res);
 	cleanedResults += "<li class='list-item'><article><header class='list-item-header'><h3 class='list-item-title'><a href='https://" + window.location.hostname + "/practice-library/practices/" +  initialResults[i].ref + "'>" + res + "</a></li></article></header></h3></header></article></li>";
 }
 
