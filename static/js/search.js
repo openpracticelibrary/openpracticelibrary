113a744cc604c9ed0186bdb2d325ef0049098137
rohitmusti <rohit.musti.rm@gmail.com>
2018-07-06 14:00:51 -0400

    fixed the search results so they look good

--- static/js/search.js
+++ static/js/search.js
@@ -19,7 +19,10 @@ var idx = lunr(function () {
 var initialResults = idx.search(searchTerm);
 
 for (var i = 0; i < initialResults.length; i++) {
-	cleanedResults += "<li class='list-item'><article><header class='list-item-header'><h3 class='list-item-title'><a href='https://" + window.location.hostname + "/practice-library/practices/" +  initialResults[i].ref + "'>" + initialResults[i].ref + "</a></li></article></header></h3></header></article></li>";
+	var res = initialResults[i].ref;
+	res = res.replace(/-/g, " ").replace(/([^a-z]|^)([a-z])(?=[a-z]{2})/g, function(_, g1, g2) { return g1 + g2.toUpperCase();});
+	console.log(res);
+	cleanedResults += "<li class='list-item'><article><header class='list-item-header'><h3 class='list-item-title'><a href='https://" + window.location.hostname + "/practice-library/practices/" +  initialResults[i].ref + "'>" + res + "</a></li></article></header></h3></header></article></li>";
 }
 
 cleanedResults += "</div>";
