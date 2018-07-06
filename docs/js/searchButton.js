fe9f3bc018d746a0724a8d5799e0f857d373824a
rohitmusti <rohit.musti.rm@gmail.com>
2018-07-06 09:47:58 -0400

    trying to modify the onclick function to work

--- docs/js/searchButton.js
+++ docs/js/searchButton.js
@@ -1,3 +1,4 @@
 document.getElementById('searchButton').onclick = function () {
 	window.location.href = window.location.hostname + "/practice-library/search/?search=" + document.getElementById("searchInput").value;
+	return false;
 };
