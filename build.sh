fd0bbe908815e9acbe02db5c5ef4f3049c2bc6e0
Ryan DeBeasi <rdebeasi@redhat.com>
2018-11-29 16:42:51 -0500

    Move scaling into build.sh

--- build.sh
+++ build.sh
@@ -31,6 +31,30 @@ _printBuildStep(){
   fi
 }
 
+scaleImages(){
+    # Scale and crop images for use in cards.
+    # If the file is below 640x426, it will be copied as-is.
+    # Depends on https://imagemagick.org/script/convert.php
+
+    ORIGINAL_PATH="static/images/"
+    SCALED_PATH="static/images/scaled/"
+
+    mkdir -p $SCALED_PATH
+    for original_file in $ORIGINAL_PATH*.{jpg,jpeg,png} ; do
+    scaled_file="${original_file/$ORIGINAL_PATH/$SCALED_PATH}"
+    if [ -f $scaled_file ]; then
+        echo Exists: $scaled_file
+    else
+        # 1. Scale each image to a minimum of 640x426, preserving aspect ratio.
+        # 2. When cropping, align to the center.
+        # 3. Crop image to exactly 640x426.
+        # The \ isn't a geometry argument; it just escapes the > character.
+        echo Creating: $scaled_file
+        convert $original_file -resize 640x426^\> -gravity center -crop 640x426+0+0 $scaled_file
+    fi
+    done
+}
+
 clean(){
     _printBuildStep "rm -rvf public static/images/scaled"
 }
@@ -42,7 +66,7 @@ test(){
 build(){
     _printBuildStep "npm i"
     _printBuildStep "./node_modules/.bin/grunt lunr-index"
-    _printBuildStep "./scale.sh"
+    _printBuildStep "./build.sh scaleImages"
     _printBuildStep "hugo"
 }
 
