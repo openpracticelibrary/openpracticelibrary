4a96f7e058fab7f92c9b68a331f03b641e542d6e
Shahein Moussavi <shaheinm@gmail.com>
2020-07-27 17:33:43 -0400

    WIP: migration -- removed dead links

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,10 +3,8 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
