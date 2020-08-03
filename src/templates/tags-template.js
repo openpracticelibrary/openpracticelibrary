6b65c21a8b3a286a2dd15b19c8df8d24481b2767
Haitham Shahin <hshahin@redhat.com>
2020-08-03 12:16:21 -0400

    removed comments

--- src/templates/tags-template.js
+++ src/templates/tags-template.js
@@ -12,60 +12,14 @@ import {
 
 const paginationLimit = 20;
 
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case "popularFilterChange":
-//       state.selectedPopularFilter = action.content;
-//       return state;
-//     case "tagFilterChange":
-//       state.selectedFilterTag = action.content;
-//       state.tagArray = formTagArray(
-//         state.selectedFilterTag,
-//         state.selectedMobiusLoopFilter
-//       );
-//       return state;
-//     case "mobiusFilterChange":
-//       state.selectedMobiusLoopFilter = action.content;
-//       state.tagArray = formTagArray(
-//         state.selectedFilterTag,
-//         state.selectedMobiusLoopFilter
-//       );
-//       return state;
-//     case "keywordSearchChange":
-//       state.keywords = action.content;
-//       return state;
-//     default:
-//       throw new Error();
-//   }
-// }
-
 const PracticesWithDataTags = (props) => {
-  // const [keywordSearchToggle, setKeywordSearchToggle] = React.useState(false);
-  // const [state, dispatch] = React.useReducer(reducer, {}, () => {
-  //   return {
-  //     tagArray: [],
-  //     selectedFilterTag: filterTags[0],
-  //     selectedMobiusLoopFilter: mobiusLoopArray[0],
-  //     selectedPopularFilter: Object.keys(popularMenuItems)[0],
-  //     keywords: [],
-  //   };
-  // });
 
   //make first letter of tag upper case (example: discovery => Discovery)
   const tag = props.pageContext.tag.charAt(0).toUpperCase() + props.pageContext.tag.slice(1);
 
-  // const toggleKeywordSearch = () =>
-  //   setKeywordSearchToggle(!keywordSearchToggle);
-
   const dataProps = {
-    // selectedPopularFilter: state.selectedPopularFilter,
-    // popularMenuItems,
-    // filterTags,
-    // selectedFilterTag: state.selectedFilterTag,
-    // keywordSearchToggle,
     mobiusLoopArray,
     selectedMobiusLoopFilter: tag,
-    // toggleKeywordSearch,
   };
 
   const {
