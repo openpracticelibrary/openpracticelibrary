253d9dfde31c910d6651ce3e2fb2a4359356a198
Lucas K Allmon <lucaskallmon@gmail.com>
2020-08-11 12:24:42 -0700

    chore(noun project): remove references

--- src/components/Learn/Partnerships.js
+++ src/components/Learn/Partnerships.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Partners from "./Partners";
 import RedHatLogo from "../../assets/images/logo-red-hat-hat-color-rgb.png";
-import NounProjectLogo from "../../assets/images/noun-project.png";
 import MobiusLogo from "../../assets/images/mobius-small.png";
 import CCHeartLogo from "../../assets/images/ccheart-black.png";
 import { Box, Typography } from "@material-ui/core";
@@ -10,29 +9,23 @@ const collaborators = [
   {
     name: "Red Hat",
     logo: RedHatLogo,
-    text: "Home of our product team and our largest Open Source Contributor."
+    text: "Home of our product team and our largest Open Source Contributor.",
   },
   {
     name: "Mobius Loop",
     logo: MobiusLogo,
     text:
-      "Our process model and partner in defining product lifecycle practices."
+      "Our process model and partner in defining product lifecycle practices.",
   },
   {
     name: "Creative Commons",
     logo: CCHeartLogo,
     text:
-      "Under this license, our content is as open source as our application software."
+      "Under this license, our content is as open source as our application software.",
   },
-  {
-    name: "The Noun Project",
-    logo: NounProjectLogo,
-    text:
-      "The community driven icon library we love so much and use for UI design."
-  }
 ];
 
-const Partnerships = props => (
+const Partnerships = (props) => (
   <Box marginTop={4} data-testid="communityHeader">
     <Typography color="primary" variant="h4" gutterBottom>
       Partnerships
