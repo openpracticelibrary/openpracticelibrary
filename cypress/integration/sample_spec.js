81b8cd415c0f911677a0acac4969701f82f7f961
Haitham Shahin <hshahin@redhat.com>
2020-08-18 09:28:47 -0400

    dynamic test for all practices

--- cypress/integration/sample_spec.js
+++
@@ -1,5 +0,0 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      cy.visit('/')
-    })
-  })
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,11 +11,33 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+const path = require("path");
+const fs = require("fs");
 
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+function readAllPractices() {
+  const directoryPath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "src",
+    "pages",
+    "practice"
+  );
+
+  var practices = [];
+
+  files = fs.readdirSync(directoryPath);
+  files.forEach(function (file) {
+    practices = [...practices, file.split(".")[0]];
+  });
+
+  return practices;
 }
+
+module.exports = (on, config) => {
+  // modify env var value
+  config.env.files = readAllPractices();
+
+  // return config
+  return config;
+};
