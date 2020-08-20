3789fde873a683821425ec3791b8201ee7b07880
Haitham Shahin <hshahin@redhat.com>
2020-08-20 11:35:14 -0400

    updated

--- cypress/integration/mobius_loop_spec.js
+++ cypress/integration/mobius_loop_spec.js
@@ -2,21 +2,21 @@ describe("Test All Mobius Loop Filters Work as Expected", () => {
   it("Verify Discovery Tag", () => {
     cy.visit("/");
     cy.get('[data-testid="Discovery"]').click();
-    cy.url().should('include','/tags/discovery')
+    cy.url().should("include", "/tags/discovery");
   });
   it("Verify Options Tag", () => {
     cy.visit("/");
     cy.get('[data-testid="Options"]').click();
-    cy.url().should('include','/tags/options')
+    cy.url().should("include", "/tags/options");
   });
   it("Verify Delivery Tag", () => {
     cy.visit("/");
     cy.get('[data-testid="Delivery"]').click();
-    cy.url().should('include','/tags/delivery')
+    cy.url().should("include", "/tags/delivery");
   });
   it("Verify Foundation Tag", () => {
     cy.visit("/");
     cy.get('[data-testid="Foundation"]').click();
-    cy.url().should('include','/tags/foundation')
+    cy.url().should("include", "/tags/foundation");
   });
 });
