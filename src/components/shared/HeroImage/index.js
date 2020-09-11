5f3c9b79bfebe53a455c4af143213355f0649b4e
Haitham Shahin <hshahin@redhat.com>
2020-09-11 10:14:22 -0400

    fix learn page image

--- src/components/shared/HeroImage/index.js
+++ src/components/shared/HeroImage/index.js
@@ -7,35 +7,60 @@ const HeroImage = ({
   children,
   opacity = 0.5,
   bgPos = "center",
-  imageUrl = DefaultImage,
+  imageUrl,
   height = "100vh",
 }) => {
-  return (
-    <BackgroundImage
-      style={{
-        backgroundSize: "cover",
-        width: "100%",
-        height: "100%",
-        backgroundPosition: `${bgPos}`,
-        backgroundRepeat: "none",
-      }}
-      fluid={[
-        `linear-gradient(rgba(0,0,0,${opacity}), rgba(0,0,0,${opacity}))`,
-        imageUrl,
-      ]}
-    >
-      <Box
-        display="flex"
-        flexDirection="row"
-        justifyContent="center"
-        alignItems="center"
-        minHeight={height}
-        flexWrap="wrap"
+  if (imageUrl) {
+    return (
+      <BackgroundImage
+        style={{
+          backgroundSize: "cover",
+          width: "100%",
+          height: "100%",
+          backgroundPosition: `${bgPos}`,
+          backgroundRepeat: "none",
+        }}
+        fluid={[
+          `linear-gradient(rgba(0,0,0,${opacity}), rgba(0,0,0,${opacity}))`,
+          imageUrl,
+        ]}
       >
-        {children}
-      </Box>
-    </BackgroundImage>
-  );
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+          minHeight={height}
+          flexWrap="wrap"
+        >
+          {children}
+        </Box>
+      </BackgroundImage>
+    );
+  } else {
+    return (
+      <Box
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
+      alignItems="center"
+      minHeight={height}
+      flexWrap="wrap"
+      style={{
+        backgroundImage: `linear-gradient(
+            rgba(0, 0, 0, ${opacity}),
+            rgba(0, 0, 0, ${opacity})
+          ), url(${DefaultImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "none",
+        backgroundPosition: `${bgPos}`,
+      }}
+    >
+      {children}
+    </Box>
+    )
+
+  }
 };
 
 export default HeroImage;
