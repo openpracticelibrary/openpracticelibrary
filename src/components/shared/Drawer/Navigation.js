bdaa9ff241e45d64beef5c9841d5b4f77662eed3
Shahein Moussavi <shaheinm@gmail.com>
2020-09-14 13:17:47 -0400

    click-through when drawer closed

--- src/components/shared/Drawer/Navigation.js
+++ src/components/shared/Drawer/Navigation.js
@@ -14,9 +14,11 @@ import DrawerFooter from "./DrawerFooter";
 
 const variantMenu = {
   open: {
+    pointerEvents: "auto",
     transition: { staggerChildren: 0.07, delayChildren: 0.2 }
   },
   closed: {
+    pointerEvents: "none",
     transition: { staggerChildren: 0.05, staggerDirection: -1 }
   }
 };
--- src/components/shared/Drawer/index.js
+++ src/components/shared/Drawer/index.js
@@ -24,7 +24,8 @@ const useStyles = makeStyles(theme => ({
     left: 0,
     bottom: 0,
     width: "300px",
-    zIndex: 10000,
+    zIndex: 1002,
+    pointerEvents: "none",
   },
   button: {
     cursor: "pointer",
@@ -37,6 +38,7 @@ const useStyles = makeStyles(theme => ({
       top: "18px",
       left: "15px",
     },
+    pointerEvents: "auto"
   }})
 );
 
@@ -91,99 +93,4 @@ const OPLDrawer = () => {
   );
 };
 
-// import React, { useReducer } from "react";
-// import { Drawer, Fab, Hidden, Box } from "@material-ui/core";
-// import MenuIcon from "@material-ui/icons/Menu";
-// import CloseIcon from "@material-ui/icons/Close";
-// import ListItems from "./ListItems";
-// import DrawerFooter from "./DrawerFooter";
-// import Logo from "../../../components/shared/Logo";
-
-// const OPLDrawer = () => {
-  // const [open, toggle] = useReducer((drawerOpen) => !drawerOpen, false);
-  // return (
-    // <>
-      // <Hidden smDown implementation="js">
-        // <Drawer
-          // variant="permanent"
-          // data-testid="drawer"
-          // PaperProps={{ elevation: 5 }}
-          // open={open}
-        // >
-          // <Box
-            // display="flex"
-            // alignItems="center"
-            // justifyContent="space-between"
-            // paddingTop={2}
-            // px={2}
-          // >
-            // {open ? (
-              // <Box marginRight={2}>
-                // <Logo small horizontal />
-              // </Box>
-            // ) : null}
-            // <Fab
-              // color="primary"
-              // disableFocusRipple
-              // data-testid="drawerActions"
-              // onClick={toggle}
-              // size="small"
-            // >
-              // {!open ? <MenuIcon /> : <CloseIcon />}
-            // </Fab>
-          // </Box>
-          // {open && (
-            // <>
-              // <ListItems drawerOpen={open} toggle={toggle} />
-              // <DrawerFooter />
-            // </>
-          // )}
-        // </Drawer>
-      // </Hidden>
-      // <Hidden mdUp>
-        // <Box position="fixed" bottom={0} m={1}>
-          // <Fab
-            // edge={false}
-            // onClick={toggle}
-            // disableFocusRipple
-            // data-testid="drawerActions"
-            // color="primary"
-          // >
-            // <MenuIcon color="" />
-          // </Fab>
-        // </Box>
-        // <Drawer
-          // variant="temporary"
-          // data-testid="drawer"
-          // open={open}
-          // transitionDuration={200}
-        // >
-          // <Box height="100vh" position="relative">
-            // <Box
-              // display="flex"
-              // alignItems="center"
-              // justifyContent="space-between"
-              // paddingTop={2}
-              // px={2}
-            // >
-              // <Logo small horizontal />
-            // </Box>
-            // <ListItems drawerOpen={open} toggle={toggle} />
-            // <DrawerFooter />
-            // <Box position="absolute" bottom={0} right={0} m={1}>
-              // <Fab
-                // onClick={toggle}
-                // disableFocusRipple
-                // data-testid="drawerActions"
-                // color="primary"
-              // >
-                // <CloseIcon />
-              // </Fab>
-            // </Box>
-          // </Box>
-        // </Drawer>
-      // </Hidden>
-    // </>
-  // );
-// };
 export default OPLDrawer;
