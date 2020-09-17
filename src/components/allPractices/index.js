2abea58927a276e9666b265a9adf8510086de83e
Lucas K Allmon <lucaskallmon@gmail.com>
2020-09-17 16:47:55 -0700

    fix(logo): hide on mobile

--- src/components/allPractices/index.js
+++ src/components/allPractices/index.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { Box, Container } from "@material-ui/core";
+import { Box, Container, Hidden } from "@material-ui/core";
 import MobiusLoopHero from "../shared/MobiusLoopHero";
 import FilterBar from "./FilterBar";
 import Logo from "../../components/shared/Logo";
 
 const Practices = ({ children, selectedMobiusLoopFilter }) => (
   <Container maxWidth="md">
-    <Box marginTop={2} marginBottom={3}>
-      <Logo small horizontal />
-    </Box>
+    <Hidden xsDown>
+      <Box marginTop={2} marginBottom={3}>
+        <Logo small horizontal />
+      </Box>
+    </Hidden>
     <MobiusLoopHero displaySection={selectedMobiusLoopFilter} />
     <Box my={{ xs: 1, md: 2, lg: 4 }}>
       <FilterBar selectedFilter={selectedMobiusLoopFilter} />
