ba7b31d337e03e80c4f14898bc3378e4d26a5d99
Lucas K Allmon <lucaskallmon@gmail.com>
2020-09-17 18:01:51 -0700

    fix(tests): repair cypress tests

--- src/components/allPractices/FilterBar.js
+++ src/components/allPractices/FilterBar.js
@@ -12,68 +12,72 @@ const FilterBar = ({ selectedFilter }) => {
         <FilterButton
           a
           color="default"
-          onClick={() => navigate("/")}
-          size="small"
-          variant="contained"
           disableElevation={selected === "all"}
           fullWidth
+          onClick={() => navigate("/")}
+          size="small"
           startIcon={mobiusContent["all"].icon}
+          variant="contained"
         >
           All
         </FilterButton>
       </Grid>
       <Grid item xs={6} md={2}>
         <FilterButton
-          onClick={() => navigate("/tags/foundation")}
           a
           color={selected === "foundation" ? "primary" : "default"}
-          fullWidth
-          variant="contained"
           disableElevation={selected === "foundation"}
+          fullWidth
+          data-testid="foundation-button"
+          onClick={() => navigate("/tags/foundation")}
           size="small"
           startIcon={mobiusContent["foundation"].icon}
+          variant="contained"
         >
           Foundation
         </FilterButton>
       </Grid>
       <Grid item xs={6} md={2}>
         <FilterButton
-          onClick={() => navigate("/tags/discovery")}
           a
           color={selected === "discovery" ? "secondary" : "default"}
-          fullWidth
-          variant="contained"
+          data-testid="discovery-button"
           disableElevation={selected === "discovery"}
+          fullWidth
+          onClick={() => navigate("/tags/discovery")}
           size="small"
           startIcon={mobiusContent["discovery"].icon}
+          variant="contained"
         >
           Discovery
         </FilterButton>
       </Grid>
       <Grid item xs={6} md={2}>
         <FilterButton
-          onClick={() => navigate("/tags/options")}
           a
           color={selected === "options" ? "options" : "default"}
-          fullWidth
-          variant="contained"
+          data-testid="options-button"
           disableElevation={selected === "options"}
+          fullWidth
+          onClick={() => navigate("/tags/options")}
           size="small"
           startIcon={mobiusContent["options"].icon}
+          variant="contained"
         >
           Options
         </FilterButton>
       </Grid>
       <Grid item xs={6} md={2}>
         <FilterButton
-          onClick={() => navigate("/tags/delivery")}
           a
           color={selected === "delivery" ? "delivery" : "default"}
-          fullWidth
-          variant="contained"
+          data-testid="delivery-button"
           disableElevation={selected === "delivery"}
+          fullWidth
+          onClick={() => navigate("/tags/delivery")}
           size="small"
           startIcon={mobiusContent["delivery"].icon}
+          variant="contained"
         >
           Delivery
         </FilterButton>
