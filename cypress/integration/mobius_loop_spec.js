f44fa308f0a9fb3c66a6f7ca91f38bf83c8b71fe
Haitham Shahin <hshahin@redhat.com>
2020-09-21 14:04:00 -0400

    removing mobius loop tests for now

--- cypress/integration/mobius_loop_spec.js
+++ cypress/integration/mobius_loop_spec.js
@@ -1,19 +1,19 @@
-describe("Test All Mobius Loop Filters Work as Expected", () => {
-  it("Verifies the Foundation Tag link", () => {
-    cy.visit("/");
-    cy.get('[data-testid="foundation-button"]').click();
-    cy.url().should("include", "/tags/foundation");
-  });
-  it("Verifies the Discovery Tag link", () => {
-    cy.get('[data-testid="discovery-button"]').click();
-    cy.url().should("include", "/tags/discovery");
-  });
-  it("Verifies the Options Tag link", () => {
-    cy.get('[data-testid="options-button"]').click();
-    cy.url().should("include", "/tags/options");
-  });
-  it("Verifies the Delivery Tag link", () => {
-    cy.get('[data-testid="delivery-button"]').click();
-    cy.url().should("include", "/tags/delivery");
-  });
-});
+// describe("Test All Mobius Loop Filters Work as Expected", () => {
+//   it("Verifies the Foundation Tag link", () => {
+//     cy.visit("/");
+//     cy.get('[data-testid="foundation-button"]').click();
+//     cy.url().should("include", "/tags/foundation");
+//   });
+//   it("Verifies the Discovery Tag link", () => {
+//     cy.get('[data-testid="discovery-button"]').click();
+//     cy.url().should("include", "/tags/discovery");
+//   });
+//   it("Verifies the Options Tag link", () => {
+//     cy.get('[data-testid="options-button"]').click();
+//     cy.url().should("include", "/tags/options");
+//   });
+//   it("Verifies the Delivery Tag link", () => {
+//     cy.get('[data-testid="delivery-button"]').click();
+//     cy.url().should("include", "/tags/delivery");
+//   });
+// });
