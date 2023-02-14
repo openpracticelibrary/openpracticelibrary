904e9802de47007d58ab2aabf4a82fc2c798b8ba
Tim Stasse <tim.stasse@gmail.com>
2023-02-14 11:53:24 +1100

    Migrated to Gatsby 3

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,9 +1,9 @@
-import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
+import { createTheme, responsiveFontSizes } from "@material-ui/core/styles";
 import MuiOverrides from "./MuiOverrides";
 // https://material-ui.com/customization/default-theme/
 // Don't override here unless you mean to do it site wide.
 
-const theme = createMuiTheme({
+const theme = createTheme({
   spacing: 8,
   breakpoints: {
     keys: ["xs", "sm", "md", "lg", "xl"],
@@ -43,16 +43,16 @@ const theme = createMuiTheme({
       main: "#76F598",
     },
     grey: {
-      "50": "#fafafa",
-      "100": "#f5f5f5",
-      "200": "#eeeeee",
-      "300": "#e0e0e0",
-      "400": "#bdbdbd",
-      "500": "#9e9e9e",
-      "600": "#757575",
-      "700": "#616161",
-      "800": "#424242",
-      "900": "#212121",
+      50: "#fafafa",
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
       A100: "#d5d5d5",
       A200: "#aaaaaa",
       A400: "#303030",
