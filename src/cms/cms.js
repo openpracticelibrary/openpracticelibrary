ce931ef6dd44e0253106920aec1247e2ad4f2ee2
Donna Benjamin <donna@kattekrab.net>
2026-03-24 15:07:37 +1100

    add cms init

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,14 +1,13 @@
 import CMS from "decap-cms-app";
+import uploadcare from "decap-cms-media-library-uploadcare";
+import cloudinary from "decap-cms-media-library-cloudinary";
+import PracticePagePreview from "./preview-templates/PracticePagePreview";
+
 if (typeof window !== 'undefined') {
   window.CMS = CMS;
   window.DecapCmsApp = CMS;
   window.NetlifyCmsApp = CMS;
-}
 
-import uploadcare from "decap-cms-media-library-uploadcare";
-import cloudinary from "decap-cms-media-library-cloudinary";
-
-import PracticePagePreview from "./preview-templates/PracticePagePreview";
 
 CMS.registerPreviewStyle(
   "https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700&display=swap"
@@ -17,5 +16,7 @@ CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("practice", PracticePagePreview);
+
+CMS.init();
 
 export default CMS;
