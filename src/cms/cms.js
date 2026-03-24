70d7ae4af0ce92e1df37818de63cf77583b8ed08
Donna Benjamin <donna@kattekrab.net>
2026-03-24 11:10:13 +1100

    define cms app

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,4 @@
-import CMS from "decap-cms-app";
+import CMS from "decap-cms-app/dist/decap-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
